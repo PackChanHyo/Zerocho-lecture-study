864790952e1be18e2a395d696fb9a0b81445066c
PackChanHyo <cksgy1510@gmail.com>
2023-03-08 16:59:19 +0900

    Nodebird_폴더구조

---
+++ nodebird/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const pageRouter = require("./routes/page");
+
+const app = express();
+app.set("port", process.env.PORT || 8001);
+app.set("view engine", "html");
+nunjucks.configure("views", {
+  express: app,
+  watch: true,
+});
+// 로깅하는것을 dev:개발모드로 한다. 나중에는 combined: 서비스 모드로 해준다.
+app.use(morgan("dev"));
+// public 파일을 자유롭게 접근할 수 있도록 한다.
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+app.use("/", pageRouter);
+app.use((req, res, next) => { // 404 NOT FOUND
+  //없는 페이지인경우
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+// 에러처리 미들웨어
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기 중");
+});
